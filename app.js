f0bf4c8d0c89fdfcc86126bb8d29972dd2295351
Nazmus Saqeeb <50136300+namoosir@users.noreply.github.com>
2021-02-16 16:07:37 -0500

    fixed animation bug

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
     });
     navLinks.forEach((link, index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+        }
     });
 }
 
