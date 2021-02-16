a0d2a3ab02382f24e5eae8f53da244bd57aad23f
Nazmus Saqeeb <50136300+namoosir@users.noreply.github.com>
2021-02-16 16:10:47 -0500

    fixed another bug

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const navSlide = () => {
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-    });
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = '';
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
-        }
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+            }
+        });
     });
 }
 
