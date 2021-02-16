807cbe8c584570c0c336b2a12188b185195d28b4
Nazmus Saqeeb <50136300+namoosir@users.noreply.github.com>
2021-02-16 16:28:34 -0500

    finally fixed all bugs

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
             }
         });
+        //burger animation
+        burger.classList.toggle('toggle');
     });
-    //burger animation
-    burger.classList.toggle('toggle');
 }
 
 navSlide();
