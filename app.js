8bd0d42fbbb33e71a969b605f7fe95c08b1f0c4b
Nazmus Saqeeb <50136300+namoosir@users.noreply.github.com>
2021-02-19 23:12:02 -0500

    added projects section

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const navSlide = () => {
+/* const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+    const menuClicks = document.querySelectorAll('.menuClick');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
@@ -15,7 +16,33 @@ const navSlide = () => {
         });
         //burger animation
         burger.classList.toggle('toggle');
+        //don't allow scroll
+        document.body.classList.toggle('noscroll');
     });
 }
+*/
 
-navSlide();
+const menuClicks = document.getElementsByClassName('menuClick');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+function animateMenu() {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.35s ease forwards ${index / 7 + 0.2}s`;
+        }
+    });
+
+    //burger animation
+    burger.classList.toggle('toggle');
+}
+
+
+burger.onclick = () => {
+    animateMenu();
+}
